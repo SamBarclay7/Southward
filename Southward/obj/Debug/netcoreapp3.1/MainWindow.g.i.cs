c35001c636f54c337b397f4062af02b4032e4302
Samo3191 <sambarclay@protonmail.com>
2021-05-09 00:56:02 +1000

    added a bunch of stuff to GUI 2

--- Southward/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ Southward/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -2397,7 +2397,7 @@ namespace Southward {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Southward;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Southward;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
